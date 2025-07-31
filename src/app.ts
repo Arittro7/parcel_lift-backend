656744d3af3ae062775549a372d2f9980ffa2feb
Arittro7 <arittrowpdev@gmail.com>
2025-07-31 17:54:49 +0600

    Custom error

--- src/app.ts
+++ src/app.ts
@@ -1,18 +1,21 @@
 import cors from "cors"
 import express, { Request, Response }  from "express"
 import { router } from "./app/routes"
+import { globalErrorHandler } from "./app/middlewares/globalErrorHandler"
 
 const app = express()
 
 app.use(express.json())
 app.use(cors())
 
-app.use("/api/v1/user", router)
+app.use("/api/v1", router)
 
 app.get("/", (req: Request, res: Response) => {
   res.status(200).json({
     message: "Welcome to 📦Parcel Lift Backend Server"
   })
 })
+
+app.use(globalErrorHandler)
 
 export default app;
