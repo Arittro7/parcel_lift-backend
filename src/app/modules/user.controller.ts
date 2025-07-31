63b97518e40b0d3c8a72d7297b7db50a7bde51e7
Arittro7 <arittrowpdev@gmail.com>
2025-07-31 18:23:04 +0600

    global err, catch async and not found

--- src/app/modules/user.controller.ts
+++ src/app/modules/user.controller.ts
@@ -2,15 +2,19 @@
 import { NextFunction, Request, Response } from "express";
 import httpStatus from "http-status-codes"
 import { userService } from "./user.service";
+import AppError from "../errorHelpers/AppError";
 
 const createUser = async(req: Request, res: Response, next: NextFunction) => {
   try {
+    throw new AppError(httpStatus.BAD_REQUEST, "Fake Error")
+    
     const user = await userService.createUser(req.body)
-
+    
     res.status(httpStatus.CREATED).json({
       message: "User Created Successfully",
       user
     })
+    
 
   } catch (err : any) {
     console.log(err);
