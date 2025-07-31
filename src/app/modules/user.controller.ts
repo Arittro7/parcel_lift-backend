31598d3644d3b099b3b02e38d3f8ffd0f2532ff4
Arittro7 <arittrowpdev@gmail.com>
2025-07-31 17:08:32 +0600

    Service layer and global err handler

--- src/app/modules/user.controller.ts
+++ src/app/modules/user.controller.ts
@@ -1,15 +1,11 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
-import { Request, Response } from "express";
-import { User } from "./user.model";
+import { NextFunction, Request, Response } from "express";
 import httpStatus from "http-status-codes"
+import { userService } from "./user.service";
 
-const createUser = async(req: Request, res: Response) => {
+const createUser = async(req: Request, res: Response, next: NextFunction) => {
   try {
-    const { name, email } = req.body
-
-    const user = await User.create({
-      name, email
-    })
+    const user = await userService.createUser(req.body)
 
     res.status(httpStatus.CREATED).json({
       message: "User Created Successfully",
@@ -18,10 +14,7 @@ const createUser = async(req: Request, res: Response) => {
 
   } catch (err : any) {
     console.log(err);
-    res.status(httpStatus.BAD_REQUEST).json({
-      message: `Something went wrong : ${err.message}`,
-      err
-    })
+    next(err)
   }
 }
 
