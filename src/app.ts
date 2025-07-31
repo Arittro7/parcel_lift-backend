7dd77ae064282357dff4c75a77c604fd4ce20c89
Arittro7 <arittrowpdev@gmail.com>
2025-07-31 15:25:41 +0600

    Project Initialize

---
+++ src/app.ts
@@ -0,0 +1,11 @@
+import express, { Request, Response }  from "express"
+
+const app = express()
+
+app.get("/", (req: Request, res: Response) => {
+  res.status(200).json({
+    message: "Welcome to 📦Parcel Lift Backend Server"
+  })
+})
+
+export default app;
---
+++ src/server.ts
@@ -0,0 +1,63 @@
+import { Server } from "http";
+import mongoose from "mongoose";
+import app from "./app";
+import { envVars } from "./app/config/env";
+
+let server: Server;
+
+const startServer = async () => {
+  try {
+    await mongoose.connect(envVars.DB_URL);
+
+    console.log("🚚 Connected to DB ⛳");
+
+    server = app.listen(envVars.PORT, () => {
+      console.log("Server is Listening to port 5000");
+    });
+
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+startServer();
+
+// ▶️ Handle Unhandled Promise Rejection
+process.on("unhandledRejection", (err) => {
+  console.log("Unhandled Rejection detected. Server is shutting down!", err);
+
+  if (server) {
+    server.close(() => {
+      process.exit(1);
+    });
+  }
+
+  process.exit(1);
+});
+
+
+// ▶️ Handle Uncaught Exception
+process.on("uncaughtException", (err) => {
+  console.log("Uncaught Exception happened. Server shutting down!", err);
+
+  if (server) {
+    server.close(() => {
+      process.exit(1);
+    });
+  }
+
+  process.exit(1);
+});
+
+
+// ▶️ Handle SIGTERM (Termination Signal from Server/Host)
+process.on("SIGTERM", () => {
+  if (server) {
+    server.close(() => {
+      process.exit(1);
+    });
+  }
+
+  process.exit(1);
+});
+
