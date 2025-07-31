c166cab7faabe354ced9ed1b3fda84877834ae96
Arittro7 <arittrowpdev@gmail.com>
2025-07-31 15:58:05 +0600

    User interface & model Schema

---
+++ src/app/modules/user.interface.ts
@@ -0,0 +1,34 @@
+import { Types } from "mongoose";
+
+export enum Role {
+  SUPER_ADMIN = "SUPER_ADMIN",
+  ADMIN = "ADMIN",
+  SENDER = "SENDER",
+  RECEIVER = "RECEIVER",
+}
+
+export enum UserStatus {
+  ACTIVE = "ACTIVE",
+  INACTIVE = "INACTIVE",
+  BLOCKED = "BLOCKED",
+}
+
+export interface IAuthProvider {
+  provider: "google" | "credentials";
+  providerId: string;
+}
+
+export interface IUser {
+  _id?: Types.ObjectId;
+  name: string;
+  email: string;
+  password: string;
+  role: Role;
+  status: UserStatus;
+  phone?: string;
+  picture?: string;
+  address?: string;
+  isDeleted?: boolean;
+  isVerified?: boolean;
+  auths: IAuthProvider[];
+}
