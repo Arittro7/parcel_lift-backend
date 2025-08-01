008adb4fa414e8c421ee513464492814d9450f6e
Arittro7 <arittrowpdev@gmail.com>
2025-08-02 02:32:26 +0600

    Create utiliti file for cookies

--- src/app/utils/usersToken.ts
+++ src/app/utils/usersToken.ts
@@ -1,6 +1,10 @@
+import  httpStatus  from 'http-status-codes';
+import { JwtPayload } from "jsonwebtoken";
 import { envVars } from "../config/env";
-import { IUser } from "../modules/User/user.interface";
-import { generateToken } from "./jwt";
+import { IUser, UserStatus} from "../modules/User/user.interface";
+import { generateToken, verifyToken } from "./jwt";
+import { User } from "../modules/User/user.model";
+import AppError from '../errorHelpers/AppError';
 
 
 export const createUsersToken = (user : Partial<IUser>) =>{
@@ -17,3 +21,29 @@ export const createUsersToken = (user : Partial<IUser>) =>{
       accessToken, refreshToken
     }
 }
+
+export const createNewAccessTokenWithRefreshToken = async (refreshToken: string) => {
+  const verifiedRefreshToken = verifyToken(refreshToken, envVars.JWT_REFRESH_SECRET) as JwtPayload;
+
+  const isUserExist = await User.findOne({ email: verifiedRefreshToken.email });
+
+  if (isUserExist?.status === UserStatus.BLOCKED || isUserExist?.status === UserStatus.INACTIVE) {
+    throw new AppError(httpStatus.BAD_REQUEST, `User is ${isUserExist.isActive}`);
+  }
+
+  if (isUserExist?.isDeleted) {
+    throw new AppError(httpStatus.BAD_REQUEST, "User is DELETED");
+  }
+
+  const jwtPayload = {
+    userId: isUserExist?._id,
+    email: isUserExist?.email,
+    role: isUserExist?.role,
+  };
+
+  const accessToken = generateToken(jwtPayload, envVars.JWT_ACCESS_SECRET, envVars.JWT_ACCESS_EXPIRES);
+
+  return {
+    accessToken
+  };
+};
